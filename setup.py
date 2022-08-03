b4b2f9f98af1d1feab566f07aa49107e88ab0dfb
Ryan Wong <rewong03@gmail.com>
2022-08-03 16:19:49 -0400

    Trying to get setup.cfg to work

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from glob import glob
+from setuptools import setup, find_packages
+
+path = os.path.join(os.path.dirname(__file__), 'src/physfix/version.py')
+with open(path, 'r') as f:
+    exec(f.read())
+
+setup(
+    version=__version__,
+    include_package_data=True
+)
